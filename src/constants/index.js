0343acbdcce89561d989dbcb0d9d9b46515a8615
Tobias Järvelöv <tobias.jarvelov@gmail.com>
2017-12-01 00:38:09 +0100

    Remove unsued const VFS_EVENT_CHANGE

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,4 +1,3 @@
-export const VFS_EVENT_CHANGE = 'VFS_EVENT_CHANGE';
 export const VFS_EVENT_MODEL_UPDATED = 'VFS_EVENT_MODEL_UPDATED';
 export const VFS_EXTERNAL_EVENT_CHANGE = 'change';
 export const VFS_EXTERNAL_EVENT_VALIDATED = 'validated';
