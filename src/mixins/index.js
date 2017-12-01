6867c27feeb5c40cc02120abf7b82d1e10932140
Tobias Järvelöv <tobias.jarvelov@gmail.com>
2017-12-01 04:58:14 +0100

    Remove core mixin, field/global mixin handles it on their own

--- src/mixins/index.js
+++ src/mixins/index.js
@@ -1,5 +1,4 @@
 import vfsBusMixin from './vfs-bus';
-import vfsCoreMixin from './vfs-core';
 import vfsDataMixin from './vfs-data';
 import vfsFieldMixin from './vfs-field';
 import vfsGlobalMixin from './vfs-global';
@@ -7,7 +6,6 @@ import vfsMethodsMixin from './vfs-methods';
 
 export {
   vfsBusMixin,
-  vfsCoreMixin,
   vfsDataMixin,
   vfsFieldMixin,
   vfsGlobalMixin,
