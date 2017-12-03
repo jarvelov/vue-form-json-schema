a62b9a4a88a403b2fa7c02bc9f4e5cc164147e40
Tobias Järvelöv <tobias.jarvelov@gmail.com>
2017-12-03 21:20:07 +0100

    Watch for changes in schema/uiSchema and create a copy for internal use

--- src/mixins/vfs-global/watch.js
+++ src/mixins/vfs-global/watch.js
@@ -2,6 +2,12 @@ const watch = {
   model(value) {
     this.vfsModel = Object.assign({}, value);
   },
+  schema(value) {
+    this.vfsSchema = Object.assign({}, value);
+  },
+  uiSchema(value) {
+    this.vfsUiSchema = value.slice(0);
+  },
 };
 
 export default watch;
