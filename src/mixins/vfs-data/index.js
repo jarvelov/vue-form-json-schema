3d2b7e527fa71264fcf555a79f34c47dc8c8bfad
Tobias Järvelöv <tobias.jarvelov@gmail.com>
2017-12-03 21:27:17 +0100

    Add vfsValidator, which is the compiled Ajv schema validator function.

--- src/mixins/vfs-data/index.js
+++ src/mixins/vfs-data/index.js
@@ -14,6 +14,7 @@ const vfsDataMixin = {
       vfsSchema: {},
       vfsUiSchema: [],
       vfsState: {},
+      vfsValidator: null,
     };
   },
 };
--- src/mixins/vfs-global/methods.js
+++ src/mixins/vfs-global/methods.js
@@ -1,4 +1,5 @@
 import Vue from 'vue';
+import Ajv from 'ajv';
 import {
   VFS_EVENT_MODEL_UPDATED,
   VFS_EXTERNAL_EVENT_CHANGE,
@@ -11,8 +12,13 @@ const methods = {
     this.vfsSchema = Object.assign({}, this.schema);
     this.vfsModel = Object.assign({}, this.model);
     this.vfsUiSchema = this.uiSchema.slice(0);
+    this.ajv = new Ajv();
 
+    this.vfsCompileSchema(this.vfsSchema);
     this.setVfsSetListeners(this.vfsEvents);
+  },
+  vfsCompileSchema(schema) {
+    this.vfsValidator = this.ajv.compile(schema);
   },
   vfsBusEventHandler(event, value) {
     if (event === VFS_EVENT_MODEL_UPDATED) {
