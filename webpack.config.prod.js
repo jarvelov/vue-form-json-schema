6034f7c215b8f5ab3b625a9f4916121655d09b8c
Tobias Järvelöv <tobias.jarvelov@gmail.com>
2017-12-04 08:24:26 +0100

    Add source map support in prod build

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -3,7 +3,10 @@ const common = require('./webpack.config.common');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = merge(common, {
+  devtool: 'source-map',
   plugins: [
-    new UglifyJSPlugin(),
+    new UglifyJSPlugin({
+      sourceMap: true,
+    }),
   ],
 });
