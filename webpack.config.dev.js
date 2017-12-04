0d830837f4323a057406acbecf3b19f2de5985c0
Tobias Järvelöv <tobias.jarvelov@gmail.com>
2017-12-04 07:22:45 +0100

    Replace cheap-eval sourcemap with inline

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -2,5 +2,5 @@ const merge = require('webpack-merge');
 const webpackBase = require('./webpack.config.common');
 
 module.exports = merge(webpackBase, {
-  devtool: 'cheap-eval-source-map',
+  devtool: 'inline-source-map',
 });
