95d08197c6da0b63bd5a31955974f13b9c17028f
Tobias Järvelöv <tobias.jarvelov@gmail.com>
2017-12-04 07:06:57 +0100

    Move mixins up a level now that constants are in a separate module

--- src/index.js
+++ src/index.js
@@ -1,9 +1,5 @@
-import * as constants from './constants';
-import * as mixins from './mixins';
-
-export {
-  constants,
-  mixins,
-};
-
-export default mixins;
+export vfsBusMixin from './vfs-bus';
+export vfsDataMixin from './vfs-data';
+export vfsFieldMixin from './vfs-field';
+export vfsGlobalMixin from './vfs-global';
+export vfsMethodsMixin from './vfs-methods';
