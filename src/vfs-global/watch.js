993838fc4576992242a0d7b8d295b9953e048d92
Tobias Järvelöv <tobias.jarvelov@gmail.com>
2017-12-06 12:06:20 +0100

    Recalculate active fields on updates to model, shema or uiSchema

--- src/vfs-global/watch.js
+++ src/vfs-global/watch.js
@@ -1,12 +1,15 @@
 const watch = {
   model(value) {
     this.vfsModel = Object.assign({}, value);
+    this.setVfsUiFieldsActive();
   },
   schema(value) {
     this.vfsSchema = Object.assign({}, value);
+    this.setVfsUiFieldsActive();
   },
   uiSchema(value) {
     this.vfsUiSchema = value.slice(0);
+    this.setVfsUiFieldsActive();
   },
 };
 
