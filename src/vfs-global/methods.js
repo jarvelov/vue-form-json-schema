ff7d4c398b96438177180d808a0d34632867e657
Tobias Järvelöv <tobias.jarvelov@gmail.com>
2017-12-07 18:14:50 +0100

    The check whether or not TO validate should not be done here, this function should only do validation, so that's what it's doing now

--- src/vfs-global/methods.js
+++ src/vfs-global/methods.js
@@ -35,20 +35,23 @@ const methods = {
   vfsBusEventHandler(event, payload) {
     const eventActions = {
       [VFS_EVENT_FIELD_MODEL_VALIDATE]: ({ key, value, cb }) => {
-        if (this.vfsOptions.validate && this.vfsOptions.validateOnChange) {
-          this.vfsBus.emit(VFS_EVENT_MODEL_VALIDATE, (errors) => {
+        const vfsModel = this.vfsHelperApplyFieldModel(key, value);
+
+        this.vfsBus.emit(VFS_EVENT_MODEL_VALIDATE, {
+          vfsModel,
+          cb: (vfsErrors) => {
             const vfsFieldErrors = this.getVfsFieldModelValidationErrors(key, value);
-            const state = this.getVfsFieldState(key);
-            const newState = Object.assign({}, state, {
+            const newState = Object.assign({}, this.getVfsFieldState(key), {
               vfsFieldErrors,
             });
+
             this.setVfsFieldState(newState, key);
 
             if (cb && typeof cb === 'function') {
               cb(vfsFieldErrors);
             }
-          });
-        }
+          },
+        });
       },
       [VFS_EVENT_FIELD_MODEL_UPDATE]: ({ key, value, cb }) => {
         this.vfsBus.emit(VFS_EVENT_FIELD_MODEL_VALIDATE, {
