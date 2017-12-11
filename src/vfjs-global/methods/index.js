158aae7fd3fcbcd1506ba134fec2e16664311018
Tobias Järvelöv <tobias.jarvelov@gmail.com>
2017-12-11 04:10:28 +0100

    Import helpers

--- src/vfjs-global/methods/index.js
+++ src/vfjs-global/methods/index.js
@@ -1,7 +1,8 @@
 import vfjsBus from './vfjs-bus';
+import vfjsHelpers from './vfjs-helpers';
+import vfjsLifecycle from './vfjs-lifecycle';
 import vfjsModel from './vfjs-model';
 import vfjsState from './vfjs-state';
-import vfjsLifecycle from './vfjs-lifecycle';
 import vfjsSchema from './vfjs-schema';
 import vfjsUi from './vfjs-ui';
 import vfjsValidation from './vfjs-validation';
@@ -9,6 +10,7 @@ import vfjsValidation from './vfjs-validation';
 const vfjsMethods = {
   ...vfjsBus,
   ...vfjsLifecycle,
+  ...vfjsHelpers,
   ...vfjsModel,
   ...vfjsSchema,
   ...vfjsState,
