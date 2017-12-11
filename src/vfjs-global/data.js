fea7993ebafb980b0554c32d70313b8731e6d007
Tobias Järvelöv <tobias.jarvelov@gmail.com>
2017-12-11 04:09:49 +0100

    export a function, not a mixin

--- src/vfjs-global/data.js
+++ src/vfjs-global/data.js
@@ -8,29 +8,25 @@ import {
   VFJS_EVENT_STATE_UPDATED,
 } from '../constants';
 
-const vfjsDataMixin = {
-  data() {
-    return {
-      vfjsBus: {},
-      vfjsComponents: {},
-      vfjsEvents: [
-        VFJS_EVENT_FIELD_MODEL_UPDATE,
-        VFJS_EVENT_FIELD_MODEL_VALIDATE,
-        VFJS_EVENT_FIELD_STATE_UPDATE,
-        VFJS_EVENT_MODEL_UPDATE,
-        VFJS_EVENT_MODEL_UPDATED,
-        VFJS_EVENT_MODEL_VALIDATE,
-        VFJS_EVENT_STATE_UPDATED,
-      ],
-      vfjsFieldsActive: [],
-      vfjsListeners: [],
-      vfjsModel: {},
-      vfjsSchema: {},
-      vfjsUiSchema: [],
-      vfjsState: {},
-      vfjsValidator: null,
-    };
-  },
-};
+const data = () => ({
+  vfjsBus: {},
+  vfjsComponents: {},
+  vfjsEvents: [
+    VFJS_EVENT_FIELD_MODEL_UPDATE,
+    VFJS_EVENT_FIELD_MODEL_VALIDATE,
+    VFJS_EVENT_FIELD_STATE_UPDATE,
+    VFJS_EVENT_MODEL_UPDATE,
+    VFJS_EVENT_MODEL_UPDATED,
+    VFJS_EVENT_MODEL_VALIDATE,
+    VFJS_EVENT_STATE_UPDATED,
+  ],
+  vfjsFieldsActive: [],
+  vfjsListeners: [],
+  vfjsModel: {},
+  vfjsSchema: {},
+  vfjsUiSchema: [],
+  vfjsState: {},
+  vfjsValidator: null,
+});
 
-export default vfjsDataMixin;
+export default data;
