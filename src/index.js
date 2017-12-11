4239dfa5670ccfa3471aabb1e6abea427d4bcc36
Tobias Järvelöv <tobias.jarvelov@gmail.com>
2017-12-11 02:36:48 +0100

    Split up vfsMethods between vfsFieldMixin and vfsGlobalMixin, since most methods are only ever used in one mixin

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 import vfsFieldMixin from './vfs-field';
 import vfsGlobalMixin from './vfs-global';
-import vfsMethodsMixin from './vfs-methods';
 import vfsViewer from './vfs-viewer';
 
 export {
   vfsFieldMixin,
   vfsGlobalMixin,
-  vfsMethodsMixin,
   vfsViewer,
 };
