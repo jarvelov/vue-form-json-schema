12a3d732ebc0d44e1bcbbef833cbc252145d4397
Tobias Järvelöv <tobias.jarvelov@gmail.com>
2017-12-11 08:23:57 +0100

    Add less verbose setters for state and model

--- src/vfjs-field/methods/setters.js
+++ src/vfjs-field/methods/setters.js
@@ -4,6 +4,12 @@ import {
 } from '../../constants';
 
 const setters = {
+  setState(value) {
+    this.setVfjsFieldState(value);
+  },
+  setModel(value) {
+    this.setVfjsFieldModel(value);
+  },
   setVfjsFieldState(value, key) {
     this.vfjsBus.$emit(VFJS_EVENT_FIELD_STATE_UPDATE, {
       key: key || this.vfjsFieldModelKey,
