90d3875e6227508861256cfcf3591745a6f7dc1e
Tobias Järvelöv <tobias.jarvelov@gmail.com>
2017-12-11 23:18:20 +0100

    Remove promises for now

--- src/vfjs-component/methods/setters.js
+++ src/vfjs-component/methods/setters.js
@@ -17,18 +17,9 @@ const setters = {
     });
   },
   setVfjsFieldModel(value, key) {
-    return new Promise((resolve, reject) => {
-      this.vfjsBus.$emit(VFJS_EVENT_FIELD_MODEL_UPDATE, {
-        key: key || this.vfjsFieldModelKey,
-        value,
-        cb: (errors) => {
-          if (errors && errors.length > 0) {
-            reject(errors);
-          }
-
-          resolve();
-        },
-      });
+    this.vfjsBus.$emit(VFJS_EVENT_FIELD_MODEL_UPDATE, {
+      key: key || this.vfjsFieldModelKey,
+      value,
     });
   },
 };
--- src/vfjs-global/methods/vfjs-model/setters.js
+++ src/vfjs-global/methods/vfjs-model/setters.js
@@ -5,18 +5,9 @@ import {
 
 const vfjsModelSetters = {
   setVfjsFieldModel(value, key) {
-    return new Promise((resolve, reject) => {
-      this.vfjsBus.$emit(VFJS_EVENT_FIELD_MODEL_UPDATE, {
-        key: key || this.vfjsFieldModelKey,
-        value,
-        cb: (errors) => {
-          if (errors && errors.length > 0) {
-            reject(errors);
-          }
-
-          resolve();
-        },
-      });
+    this.vfjsBus.$emit(VFJS_EVENT_FIELD_MODEL_UPDATE, {
+      key: key || this.vfjsFieldModelKey,
+      value,
     });
   },
   setVfjsModel(model) {
