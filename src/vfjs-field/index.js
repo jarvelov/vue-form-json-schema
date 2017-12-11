b53ee8467afdd7deef823f61cb11ca2738628808
Tobias Järvelöv <tobias.jarvelov@gmail.com>
2017-12-11 03:04:27 +0100

    Remove removed mixin

--- src/vfjs-field/index.js
+++ src/vfjs-field/index.js
@@ -1,10 +1,8 @@
 import data from './data';
 import methods from './methods';
 import props from './props';
-import vfjsMethodsMixin from '../vfjs-methods';
 
 const vfjsFieldMixin = {
-  mixins: [vfjsMethodsMixin],
   data,
   props,
   methods,
