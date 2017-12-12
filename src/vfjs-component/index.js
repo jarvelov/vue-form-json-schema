c6cb7de34955e3db0d1ad3b2c3b6b97774d4ba25
Tobias Järvelöv <tobias.jarvelov@gmail.com>
2017-12-12 16:08:05 +0100

    Remove the blur listeners as a default listener

--- src/vfjs-component/index.js
+++ src/vfjs-component/index.js
@@ -3,12 +3,6 @@ import methods from './methods';
 import props from './props';
 
 const vfjsFieldMixin = {
-  mounted() {
-    this.vfjsFieldHelperAddListener(this.$el, 'blur');
-  },
-  beforeDestroy() {
-    this.vfjsFieldHelperRemoveListener(this.$el, 'blur');
-  },
   computed,
   props,
   methods,
