c473e0f4b7d72ad5167b2b9104af26e0904bb28d
Tobias Järvelöv <tobias.jarvelov@gmail.com>
2017-12-12 01:38:22 +0100

    When setting ui schema, generate a uuid for each field to prevent unnecessary re-rendering

--- src/vfjs-global/methods/vfjs-ui/setters.js
+++ src/vfjs-global/methods/vfjs-ui/setters.js
@@ -1,4 +1,10 @@
 const vfjsUiSetters = {
+  setVfjsUiSchema(uiSchema) {
+    this.vfjsUiSchema = [...uiSchema].reduce((fields, field) => ([
+      ...fields,
+      this.vfjsHelpersGenerateFieldUuid(field),
+    ]), []);
+  },
   setVfjsUiFieldsActive() {
     this.vfjsFieldsActive = this.vfjsUiSchema.reduce((fields, field) => ([
       ...fields,
