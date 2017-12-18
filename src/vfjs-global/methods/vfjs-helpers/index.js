c5743b65e6db5a45659d7ca1dcccb262c7e12c27
Tobias Järvelöv <tobias.jarvelov@gmail.com>
2017-12-18 11:15:13 +0100

    In order for a component to be updated when the children does, we need to generate a hash with them which is then used as the key to save the rendered node to.

--- src/vfjs-global/methods/vfjs-helpers/index.js
+++ src/vfjs-global/methods/vfjs-helpers/index.js
@@ -51,7 +51,11 @@ const vfjsHelpers = {
       vfjsState,
     };
 
+    const childString = JSON.stringify(children);
+    const childId = this.vfjsHelperHashString(childString);
+
     return this.vfjsHelperCreateComponent({
+      id: `${vfjsFieldId}-${childId}`,
       component,
       children: fieldChildren,
       props,
