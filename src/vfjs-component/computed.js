03ca7251e284bbd2ffd4841cc3eb49717f1fd023
Tobias Järvelöv <tobias.jarvelov@gmail.com>
2017-12-19 10:09:26 +0100

    If the component is a string and is one of the elements which supports the 'value' attribute we autofill the innerHTML and value attribute

--- src/vfjs-component/computed.js
+++ src/vfjs-component/computed.js
@@ -2,13 +2,47 @@ import { merge } from 'lodash';
 
 const computed = {
   vfjsAttributes() {
+    const attrs = {
+      required: this.vfjsFieldRequired,
+    };
+
+    const domProps = {
+      required: this.vfjsFieldRequired,
+    };
+
+    if (this.component) {
+      // Elements which supports the 'value' attribute
+      // taken from: https://developer.mozilla.org/en-US/docs/Web/HTML/Attributes
+      const elements = [
+        'button',
+        'input',
+        'li',
+        'meter',
+        'option',
+        'param',
+        'progress',
+      ];
+
+      if (elements.indexOf(this.component) !== -1) {
+        domProps.innerHTML = (
+          this.vfjsFieldModel ||
+          (this.vfjsFieldOptions.domProps && this.vfjsFieldOptions.domProps.innerHTML)
+        );
+
+        attrs.value = (
+          this.vfjsFieldModel ||
+          (this.vfjsFieldOptions.attrs && this.vfjsFieldOptions.attrs.value) ||
+          domProps.innerHTML
+        );
+      }
+    }
+
     return {
       ...this.getVfjsFieldAttributes(
         this.vfjsFieldOptions,
         merge({}, this.vfjsDefaultOptions, {
-          attrs: {
-            required: this.vfjsFieldRequired,
-          },
+          attrs,
+          domProps,
           key: this.vfjsFieldId,
           props: this.$options.propsData,
         }),
