73ca14d3ab2456b705b4206dd8432573d8657de2
Tobias Järvelöv <tobias.jarvelov@gmail.com>
2017-12-20 16:49:50 +0100

    Add schemas before call to validation

--- src/vfjs-global/methods/vfjs-validation/index.js
+++ src/vfjs-global/methods/vfjs-validation/index.js
@@ -7,15 +7,15 @@ import {
 const vfjsValidation = {
   vfjsValidationInitialize() {
     this.ajv = new Ajv();
+    // Add additional schemas
+    Object.keys(this.schemas).forEach((key) => {
+      this.ajv.addSchema(this.schemas[key], key);
+    });
 
     // Check if validation is enabled and set to run on load
     if (this.vfjsOptions.validate && this.vfjsOptions.validateOnLoad) {
       this.vfjsBus.$emit(VFJS_EVENT_MODEL_VALIDATE, { vfjsModel: this.getVfjsModel() });
     }
-
-    Object.keys(this.schemas).forEach((key) => {
-      this.ajv.addSchema(this.schemas[key], key);
-    });
   },
   ...getters,
 };
