bafac9041bcf407777f5632cd5d2d135286926b5
Tobias Järvelöv <tobias.jarvelov@gmail.com>
2017-12-24 10:06:05 +0100

    "debounce" updates if there were less than 150ms between umodel updates

--- src/vfjs-component/methods/setters.js
+++ src/vfjs-component/methods/setters.js
@@ -17,10 +17,24 @@ const setters = {
     });
   },
   setVfjsFieldModel(value, key) {
-    this.vfjsBus.$emit(VFJS_EVENT_FIELD_MODEL_UPDATE, {
-      key: key || this.vfjsFieldModelKey,
-      value,
-    });
+    if (this.timeout) {
+      clearTimeout(this.timeout);
+    }
+
+    // Save timestamp and make sure that there was at least 150 ms between each update
+    // If there was more than 150ms since last update set delay to 0
+    const timestamp = Date.now();
+    const diff = this.vfjsFieldState.updatedAt ? (timestamp - this.vfjsFieldState.updatedAt) : 0;
+    const delay = this.vfjsFieldState.updatedAt
+      ? Math.min(diff, 150)
+      : 0;
+
+    this.timeout = setTimeout(() => {
+      this.vfjsBus.$emit(VFJS_EVENT_FIELD_MODEL_UPDATE, {
+        key: key || this.vfjsFieldModelKey,
+        value,
+      });
+    }, delay);
   },
 };
 
