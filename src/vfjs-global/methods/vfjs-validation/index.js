488fdb9f9395f663c0422853d92fe0973369a677
Tobias Järvelöv <tobias.jarvelov@gmail.com>
2017-12-24 10:39:58 +0100

    Save all the required fields in the vfjsFieldsRequired array

--- src/vfjs-global/methods/vfjs-validation/index.js
+++ src/vfjs-global/methods/vfjs-validation/index.js
@@ -18,6 +18,32 @@ const vfjsValidation = {
       this.ajv.addSchema(this.schemas[key], key);
     });
 
+    // TODO: Move this to a method so we can call it when the schema/ui-schema/model is updated
+
+    // This is somewhat of a hack...
+    //
+    // To find out if a field is required
+    // we get the schema and use an empty object
+    // as the data, with allErrors option in Ajv
+    // we can get all the required properties
+    // and check if the model key is found in the errors
+    this.ajv.validate(this.getVfjsSchema(), {});
+
+    this.vfjsFieldsRequired = this.ajv.errors
+      ? this.ajv.errors.reduce((required, error) => {
+        if (error.keyword === 'required') {
+          if (error.params && error.params.missingProperty) {
+            if (required.indexOf(error.params.missingProperty) === -1) {
+              required.push(error.params.missingProperty);
+            }
+          }
+        }
+
+        return required;
+      }, [])
+      : [];
+
+
     // Check if validation is enabled and set to run on load
     if (this.vfjsOptions.validate && this.vfjsOptions.validateOnLoad) {
       this.vfjsBus.$emit(VFJS_EVENT_MODEL_VALIDATE, { vfjsModel: this.getVfjsModel() });
