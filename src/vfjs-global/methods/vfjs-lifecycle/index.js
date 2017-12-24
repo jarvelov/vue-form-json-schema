494aaabd5bfeaa3b3a4ce297e5b0fa54818c3edb
Tobias Järvelöv <tobias.jarvelov@gmail.com>
2017-12-24 09:55:17 +0100

    Reorder

--- src/vfjs-global/methods/vfjs-lifecycle/index.js
+++ src/vfjs-global/methods/vfjs-lifecycle/index.js
@@ -3,28 +3,29 @@ const vfjsLifecycle = {
     this.vfjsEvents.forEach(event => this.removeVfjsListener(event));
   },
   vfjsInitialize() {
+    // Set the JSON schema
+    this.vfjsSchema = Object.assign({}, this.schema);
+
+    // Set the available components
+    this.vfjsComponents = Object.assign({}, this.components);
+
+    // Set up options
+    this.vfjsOptions = Object.assign({}, this.vfjsOptions, this.options);
+
     // Set up the plugin's internal bus
     this.vfjsBusInitialize();
 
     // Set the model from props
     this.setVfjsModel(this.model);
 
-    // Set the available components
-    this.vfjsComponents = Object.assign({}, this.components);
+    // Set up validation
+    this.vfjsValidationInitialize();
 
-    // vfjsVnodes holds the rendered components so we can reuse them
-    // without having to create a new component every time something changes
-    this.vfjsVnodes = {};
-
-    // Set the JSON schema
-    this.vfjsSchema = Object.assign({}, this.schema);
+    // Set up ui schema
     this.setVfjsUiSchema(this.uiSchema);
 
     // Register events in vfjsEvents to vfjsBusEventHandler
     this.addVfjsListeners(this.vfjsEvents, this.vfjsBusEventHandler);
-
-    // Set up validation
-    this.vfjsValidationInitialize();
 
     // Check and set active fields (visible)
     this.setVfjsUiFieldsActive();
