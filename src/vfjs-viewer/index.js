e00740873636281494490d02d0641291d0797df6
Tobias Järvelöv <tobias.jarvelov@gmail.com>
2017-12-24 10:10:38 +0100

    Remove support for validationClass

--- src/vfjs-viewer/index.js
+++ src/vfjs-viewer/index.js
@@ -3,25 +3,8 @@ import vfjsGlobalMixin from '../vfjs-global';
 const VueFormJsonSchemaViewer = {
   name: 'vue-form-json-schema-viewer',
   mixins: [vfjsGlobalMixin],
-  computed: {
-    validated() {
-      return this.getVfjsValid();
-    },
-    vfjsAttributes() {
-      return {
-        class: this.vfjsAttributesClass,
-      };
-    },
-    vfjsAttributesClass() {
-      return (this.options.validationClass && this.validated)
-        ? this.options.validationClass
-        : '';
-    },
-  },
   render() {
-    return this.$createElement(this.tag, {
-      ...this.vfjsAttributes,
-    }, this.vfjsFields);
+    return this.$createElement(this.tag, this.vfjsFields);
   },
 };
 
