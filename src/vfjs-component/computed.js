e230f8b6fbbc7bafdfef2f5f16ad2bc7dfec33d8
Tobias Järvelöv <tobias.jarvelov@gmail.com>
2017-12-24 12:04:11 +0100

    Remove unused code

--- src/vfjs-component/computed.js
+++ src/vfjs-component/computed.js
@@ -54,19 +54,6 @@ const computed = {
     );
   },
   vfjsAttributes() {
-    const attrs = {
-      // id: this.vfjsFieldId, // This is very useful when debugging to see when nodes are updated
-      required: this.vfjsFieldRequired,
-    };
-
-    if (elements.indexOf(this.component) !== -1) {
-      attrs.value = (
-        this.vfjsFieldModel ||
-        (this.vfjsFieldOptions.attrs && this.vfjsFieldOptions.attrs.value) ||
-        (this.vfjsFieldOptions.domProps && this.vfjsFieldOptions.domProps.innerHTML)
-      );
-    }
-
     return {
       ...this.getVfjsFieldAttributes(
         this.vfjsFieldOptions,
