12088f5bed9dea3de171463f12caa69cd9e606df
Tobias Järvelöv <tobias.jarvelov@gmail.com>
2018-01-08 17:58:37 +0100

    Remove unused methods

--- src/vfjs-global/methods/vfjs-validation/getters.js
+++ src/vfjs-global/methods/vfjs-validation/getters.js
@@ -1,25 +1,6 @@
 import { get } from 'lodash';
 
 const vfjsValidationGetters = {
-  getVfjsFieldUiSchemaValidationErrors({ component, model, type }) {
-    const errors = [];
-
-    if (!component && !this.getVfjsFieldComponent(type)) {
-      errors.push(`Unregistered component type: ${type}`);
-    }
-
-    if (model && !(String(model) === model)) {
-      errors.push('The field\'s "model" property must be a string!');
-    }
-
-    return errors;
-  },
-  getVfjsFieldUiSchemaValid(uiSchema) {
-    return (
-      this.getVfjsFieldUiSchemaValidationErrors(uiSchema).length === 0 &&
-      uiSchema.children.every(this.getVfjsFieldUiSchemaValid)
-    );
-  },
   getVfjsFieldModelValid(key, value) {
     const errors = this.getVfjsFieldModelValidationErrors(key, value);
     return errors.length === 0;
