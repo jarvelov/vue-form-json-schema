386893d921e2347d44f43733073115c956fe0dde
Tobias Järvelöv <tobias.jarvelov@gmail.com>
2018-01-09 09:29:34 +0100

    Only perform validation when the value is true

--- src/vfjs-global/watch.js
+++ src/vfjs-global/watch.js
@@ -22,39 +22,46 @@ const watch = {
     this.setVfjsUiFieldsActive();
   },
   showValidationErrors(value) {
-    this.vfjsBus.$emit(VFJS_EVENT_MODEL_VALIDATE, {
-      vfjsModel: this.getVfjsModel(),
-      cb: () => {
-        const validateRequired = key => new Promise((resolve, reject) => {
-          this.vfjsBus.$emit(VFJS_EVENT_FIELD_MODEL_VALIDATE, {
-            key,
-            value: this.getVfjsFieldModel(key),
-            cb: (vfjsFieldErrors) => {
-              const fieldState = this.getVfjsFieldState(key);
-              this.vfjsBus.$emit(VFJS_EVENT_FIELD_STATE_UPDATE, {
-                key,
-                value: {
-                  ...fieldState,
-                  vfjsFieldErrors,
-                },
-                cb: () => {
-                  resolve();
-                },
-              });
-            },
-          });
-        });
-
-        const requiredValidations = this.vfjsFieldsRequired.map(validateRequired);
-
-        Promise.all(requiredValidations)
-          .then(() => {
-            this.vfjsBus.$emit(VFJS_EVENT_STATE_UPDATE, {
-              key: 'showValidationErrors',
-              value,
+    if (value) {
+      this.vfjsBus.$emit(VFJS_EVENT_MODEL_VALIDATE, {
+        vfjsModel: this.getVfjsModel(),
+        cb: () => {
+          const validateRequired = key => new Promise((resolve, reject) => {
+            this.vfjsBus.$emit(VFJS_EVENT_FIELD_MODEL_VALIDATE, {
+              key,
+              value: this.getVfjsFieldModel(key),
+              cb: (vfjsFieldErrors) => {
+                const fieldState = this.getVfjsFieldState(key);
+                this.vfjsBus.$emit(VFJS_EVENT_FIELD_STATE_UPDATE, {
+                  key,
+                  value: {
+                    ...fieldState,
+                    vfjsFieldErrors,
+                  },
+                  cb: () => {
+                    resolve();
+                  },
+                });
+              },
             });
           });
-      },
+
+          const requiredValidations = this.vfjsFieldsRequired.map(validateRequired);
+
+          return Promise.all(requiredValidations)
+            .then(() => {
+              this.vfjsBus.$emit(VFJS_EVENT_STATE_UPDATE, {
+                key: 'showValidationErrors',
+                value,
+              });
+            });
+        },
+      });
+    }
+
+    this.vfjsBus.$emit(VFJS_EVENT_STATE_UPDATE, {
+      key: 'showValidationErrors',
+      value,
     });
   },
 };
