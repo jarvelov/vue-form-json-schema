522a71745997b8e6987d772904f5ad9d47fe3344
Tobias Järvelöv <tobias.jarvelov@gmail.com>
2018-12-27 01:15:10 +0100

    Expose plugin

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import vfjsField from './vfjs-field-component';
 import vfjsFieldMixin from './vfjs-field-mixin';
 import vfjsGlobalMixin from './vfjs-global-mixin';
 import vfjsGlobal from './vfjs-global-component';
+import vfjsPlugin from './plugin';
 
 export {
   vfjsField,
   vfjsFieldMixin,
   vfjsGlobal,
   vfjsGlobalMixin,
+  vfjsPlugin,
 };
 
 export default vfjsGlobal;
