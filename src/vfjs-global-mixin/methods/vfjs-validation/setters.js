05588ea153da0e625c476dceda7b60d6885ccc30
Tobias Järvelöv <tobias.jarvelov@gmail.com>
2018-12-27 09:35:28 +0100

    Format

--- src/vfjs-global-mixin/methods/vfjs-validation/setters.js
+++ src/vfjs-global-mixin/methods/vfjs-validation/setters.js
@@ -10,26 +10,25 @@ const vfjsValidationSetters = {
     this.vfjsBus.emit(VFJS_EVENT_MODEL_VALIDATE, {
       vfjsModel: this.getVfjsModel(),
       cb: () => {
-        const validateRequired = key =>
-          new Promise((resolve, reject) => {
-            this.vfjsBus.emit(VFJS_EVENT_FIELD_MODEL_VALIDATE, {
-              key,
-              value: this.getVfjsFieldModel(key),
-              cb: (vfjsFieldErrors) => {
-                const fieldState = this.getVfjsFieldState(key);
-                this.vfjsBus.emit(VFJS_EVENT_FIELD_STATE_UPDATE, {
-                  key,
-                  value: {
-                    ...fieldState,
-                    vfjsFieldErrors,
-                  },
-                  cb: () => {
-                    resolve();
-                  },
-                });
-              },
-            });
+        const validateRequired = key => new Promise((resolve, reject) => {
+          this.vfjsBus.emit(VFJS_EVENT_FIELD_MODEL_VALIDATE, {
+            key,
+            value: this.getVfjsFieldModel(key),
+            cb: (vfjsFieldErrors) => {
+              const fieldState = this.getVfjsFieldState(key);
+              this.vfjsBus.emit(VFJS_EVENT_FIELD_STATE_UPDATE, {
+                key,
+                value: {
+                  ...fieldState,
+                  vfjsFieldErrors,
+                },
+                cb: () => {
+                  resolve();
+                },
+              });
+            },
           });
+        });
 
         const operations = this.vfjsFieldsRequired.map(validateRequired);
         return Promise.all(operations).then(() => this.vfjsBus.emit(VFJS_EVENT_UI_FIELDS_UPDATE));
