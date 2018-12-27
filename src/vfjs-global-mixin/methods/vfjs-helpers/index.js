01d6b18154f6d43d24b4961a4dbad94d3609a4a4
Tobias Järvelöv <tobias.jarvelov@gmail.com>
2018-12-27 01:34:40 +0100

    Add eslint opt outs

--- src/vfjs-global-mixin/methods/vfjs-helpers/index.js
+++ src/vfjs-global-mixin/methods/vfjs-helpers/index.js
@@ -200,8 +200,10 @@ const vfjsHelpers = {
     return fields.reduce((models, { children = [], displayOptions = {}, model }) => {
       if (displayOptions.clearOnHide) {
         if (model) {
+          // eslint-disable-next-line no-param-reassign
           models[model] = displayOptions.clearOnHide;
         } else if (!model && typeof displayOptions.clearOnHide === 'string') {
+          // eslint-disable-next-line no-param-reassign
           models[displayOptions.clearOnHide] = displayOptions.clearOnHide;
         }
       }
