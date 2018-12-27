d05da752402d6990eba1d2cca210c10aa855ce8c
Tobias Järvelöv <tobias.jarvelov@gmail.com>
2018-12-27 01:14:21 +0100

    Fix to handle automatic registration of vue-form-json-schema when used with a script tag in a browser

--- src/plugin.js
+++ src/plugin.js
@@ -1,14 +1,32 @@
 import vfjsGlobalComponent from './vfjs-global-component';
 
-const plugin = {
-  install(
-    Vue,
-    options = {
-      component: 'vue-form-json-schema',
-    },
-  ) {
-    Vue.component(options.component, vfjsGlobalComponent);
+// Declare install function executed by Vue.use()
+export function install(
+  Vue,
+  options = {
+    component: 'vue-form-json-schema',
   },
+) {
+  if (!install.installed) {
+    install.installed = true;
+    Vue.component(options.component, vfjsGlobalComponent);
+  }
+}
+
+// Create module definition for Vue.use()
+const plugin = {
+  install,
 };
+
+// Auto-install when vue is found (eg. in browser via <script> tag)
+let GlobalVue = null;
+if (typeof window !== 'undefined') {
+  GlobalVue = window.Vue;
+} else if (typeof global !== 'undefined') {
+  GlobalVue = global.Vue;
+}
+if (GlobalVue) {
+  GlobalVue.use(plugin);
+}
 
 export default plugin;
