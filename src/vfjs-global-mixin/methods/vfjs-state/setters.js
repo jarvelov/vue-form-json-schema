4fe84e9d9d0328dacc550589bc1219c68d389b9d
Tobias Järvelöv <tobias.jarvelov@gmail.com>
2019-01-16 23:13:49 +0100

    Compare against value returned from getter

--- src/vfjs-global-mixin/methods/vfjs-state/setters.js
+++ src/vfjs-global-mixin/methods/vfjs-state/setters.js
@@ -12,7 +12,7 @@ const vfjsStateSetters = {
     });
   },
   setVfjsState(state) {
-    if (!isEqual(state, this.vfjsState)) {
+    if (!isEqual(state, this.getVfjsState())) {
       this.vfjsState = Object.assign({}, this.getVfjsState(), state);
       this.vfjsBus.emit(VFJS_EVENT_STATE_UPDATED, () => {
         this.setVfjsFields();
