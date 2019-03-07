84ec66e5cbc1142e7c801246dce42fea09971aa7
Tobias Järvelöv <tobias.jarvelov@gmail.com>
2019-03-07 10:35:53 +0100

    Declare the componentProperties property on vfjsOptions so the user can override it

--- src/vfjs-global-mixin/data.js
+++ src/vfjs-global-mixin/data.js
@@ -30,7 +30,6 @@ const data = () => ({
   vfjsFieldsRequired: [],
   vfjsModel: {},
   vfjsOptions: {
-    allowInvalidModel: true,
     ajv: {
       keywords: {},
       locale: null,
@@ -38,7 +37,32 @@ const data = () => ({
         allErrors: true,
       },
     },
+    allowInvalidModel: true,
     castToSchemaType: false,
+    componentProperties: {
+      attrs: {
+        // Components which support the 'checked' attribute
+        checked: ['checkbox', 'radio'],
+        // Components which support the 'required' attribute
+        required: ['input', 'select', 'textarea'],
+        // Components which support the 'value' attribute
+        value: ['input', 'option', 'textarea'],
+      },
+      domProps: {
+        // Components which should have its DOM property 'checked' updated
+        checked: ['checkbox', 'radio'],
+        // Components which should have its DOM property 'required' updated
+        required: ['input', 'select', 'textarea'],
+        // Components which should have its DOM property 'value' updated
+        value: ['input', 'textarea'],
+      },
+      props: {
+        // Components which support the 'required' prop
+        required: [],
+      },
+      // Components which has their value within the tags
+      innerHTML: ['textarea'],
+    },
     showValidationErrors: false,
     validate: true,
     validateOnLoad: true,
