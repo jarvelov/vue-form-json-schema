c8fd1870d730a65b8ec4b7911dc99aadf1510cc1
Tobias Järvelöv <tobias.jarvelov@gmail.com>
2019-03-07 10:29:19 +0100

    Move the settings for which components supports which dom props/attrs etc. to the componentProperties property on the vfjsOptions prop.

--- src/vfjs-field-mixin/computed.js
+++ src/vfjs-field-mixin/computed.js
@@ -1,20 +1,5 @@
 import { merge } from 'lodash';
 
-// Elements which supports the 'value' attribute
-const attrsValueElements = ['input', 'option', 'textarea'];
-
-// Elements which should have its DOM property 'value' updated
-const domPropsValueElements = ['input', 'textarea'];
-
-// Elements which should have its DOM property 'checked' updated
-const domPropsCheckedElements = ['checkbox', 'radio'];
-
-// Elements which has their value within the tags
-const innerHTMLElements = ['textarea'];
-
-// Elements which supports the 'required' attribute
-const requiredElements = ['input', 'select', 'textarea'];
-
 const computed = {
   vfjsComputedFieldHasErrors() {
     return this.vfjsFieldState.vfjsFieldErrors && this.vfjsFieldState.vfjsFieldErrors.length > 0;
@@ -35,11 +20,13 @@ const computed = {
       // id: this.vfjsFieldId, // This is very useful when debugging to see when nodes are updated
     };
 
-    if (requiredElements.indexOf(this.vfjsComponent) !== -1) {
+    const { required = [], value = [] } = this.vfjsOptions.componentProperties.attrs;
+
+    if (required.indexOf(this.vfjsComponent) !== -1) {
       attrs.required = this.vfjsFieldRequired;
     }
 
-    if (attrsValueElements.indexOf(this.vfjsComponent) !== -1) {
+    if (value.indexOf(this.vfjsComponent) !== -1) {
       attrs.value = this.vfjsFieldModel || (this.vfjsFieldOptions.attrs && this.vfjsFieldOptions.attrs.value);
     }
 
@@ -48,28 +35,44 @@ const computed = {
   vfjsComputedFieldDomProps() {
     const domProps = {};
 
-    if (innerHTMLElements.indexOf(this.vfjsComponent) !== -1) {
+    const { innerHTML = [] } = this.vfjsOptions.componentProperties;
+
+    if (innerHTML.indexOf(this.vfjsComponent) !== -1) {
       domProps.innerHTML = this.vfjsFieldModel
         || (this.vfjsFieldOptions.domProps && this.vfjsFieldOptions.domProps.innerHTML);
     }
 
-    if (domPropsValueElements.indexOf(this.vfjsComponent) !== -1) {
+    const { checked, value } = this.vfjsOptions.componentProperties.domProps;
+
+    if (value.indexOf(this.vfjsComponent) !== -1) {
       domProps.value = this.vfjsFieldModel
         || (this.vfjsFieldOptions.domProps && this.vfjsFieldOptions.domProps.value);
     }
 
-    if (domPropsCheckedElements.indexOf(this.vfjsComponent) !== -1) {
+    if (checked.indexOf(this.vfjsComponent) !== -1) {
       domProps.checked = this.vfjsFieldModel
         || (this.vfjsFieldOptions.domProps && this.vfjsFieldOptions.domProps.checked);
     }
 
     return domProps;
   },
+  vfjsComputedFieldProps() {
+    const props = {};
+
+    const { props: componentProps } = this.vfjsOptions.componentProperties.domProps;
+
+    if (componentProps.indexOf(this.vfjsComponent) !== -1) {
+      props.required = this.vfjsFieldRequired;
+    }
+
+    return props;
+  },
   vfjsComputedFieldOptions() {
     return {
       attrs: this.vfjsComputedFieldAttrs,
       domProps: this.vfjsComputedFieldDomProps,
       key: this.vfjsFieldOptions.key || this.vfjsFieldId,
+      props: this.vfjsComputedFieldProps,
     };
   },
   vfjsComputedMergedFieldOptions() {
