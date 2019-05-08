84ea03cf982a5771a4009cfd748c91aeb2c0db3e
Tobias Järvelöv <tobias.jarvelov@gmail.com>
2019-05-08 22:54:08 +0200

    Don't try to access key on subObj unless it is truthy

--- src/helpers/get.js
+++ src/helpers/get.js
@@ -1,7 +1,7 @@
 const get = (obj, path) => {
   if (obj !== null && typeof obj === 'object') {
     const paths = String(path).split('.');
-    return paths.reduce((subObj, key) => subObj[key], obj);
+    return paths.reduce((subObj, key) => subObj && subObj[key], obj);
   }
 
   return null;
