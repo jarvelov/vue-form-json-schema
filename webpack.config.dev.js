1f29b32147c03b5fbdce11197d8b12bfa3d9a0de
Tobias Järvelöv <tobias.jarvelov@gmail.com>
2019-12-02 22:04:01 +0100

    Build UMD build for dev as well

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,16 +1,30 @@
 const path = require('path');
 const merge = require('webpack-merge');
 const nodeExternals = require('webpack-node-externals');
-const webpackBase = require('./webpack.config.common');
+const common = require('./webpack.config.common');
 
-module.exports = merge(webpackBase, {
-  entry: path.resolve(__dirname, './src/index.js'),
-  devtool: 'inline-source-map',
-  mode: 'development',
-  externals: [nodeExternals()],
-  output: {
-    path: path.resolve(__dirname, './dist'),
-    filename: 'vue-form-json-schema.esm.js',
-    libraryTarget: 'commonjs2',
-  },
-});
+module.exports = [
+  merge(common, {
+    entry: path.resolve(__dirname, './src/index.js'),
+    devtool: 'inline-source-map',
+    mode: 'development',
+    output: {
+      path: path.resolve(__dirname, './dist'),
+      filename: 'vue-form-json-schema.umd.js',
+      libraryTarget: 'umd',
+      library: 'VueFormJsonSchema',
+      umdNamedDefine: true,
+    },
+  }),
+  merge(common, {
+    entry: path.resolve(__dirname, './src/index.js'),
+    devtool: 'inline-source-map',
+    mode: 'development',
+    externals: [nodeExternals()],
+    output: {
+      path: path.resolve(__dirname, './dist'),
+      filename: 'vue-form-json-schema.esm.js',
+      libraryTarget: 'commonjs2',
+    },
+  }),
+];
