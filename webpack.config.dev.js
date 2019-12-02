ed81997c57558d3699792610ea1d3b88337a7ab0
Tobias Järvelöv <tobias.jarvelov@gmail.com>
2019-12-02 22:10:04 +0100

    Build UMD build in dev

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,16 +1,31 @@
 const path = require('path');
 const merge = require('webpack-merge');
 const nodeExternals = require('webpack-node-externals');
-const webpackBase = require('./webpack.config.common');
+const common = require('./webpack.config.common');
 
-module.exports = merge(webpackBase, {
+const development = {
   entry: path.resolve(__dirname, './src/index.js'),
   devtool: 'inline-source-map',
   mode: 'development',
-  externals: [nodeExternals()],
-  output: {
-    path: path.resolve(__dirname, './dist'),
-    filename: 'vue-form-json-schema.esm.js',
-    libraryTarget: 'commonjs2',
-  },
-});
+};
+
+module.exports = [
+  merge(common, development, {
+    output: {
+      path: path.resolve(__dirname, './dist'),
+      filename: 'vue-form-json-schema.umd.js',
+      libraryTarget: 'umd',
+      library: 'VueFormJsonSchema',
+      umdNamedDefine: true,
+    },
+  }),
+  merge(common, development, {
+    externals: [nodeExternals()],
+    output: {
+      path: path.resolve(__dirname, './dist'),
+      filename: 'vue-form-json-schema.esm.js',
+      libraryTarget: 'commonjs2',
+      library: 'VueFormJsonSchema',
+    },
+  }),
+];
