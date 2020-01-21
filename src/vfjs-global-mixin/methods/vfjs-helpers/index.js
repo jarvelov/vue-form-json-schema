0d4ae67468ec12cfe808b658680e4f2bbb8e12ee
Tobias Järvelöv <tobias.jarvelov@gmail.com>
2020-01-21 22:10:56 +0100

    Refactor getVfjsFieldsModels to be more readable

--- src/vfjs-global-mixin/methods/vfjs-helpers/index.js
+++ src/vfjs-global-mixin/methods/vfjs-helpers/index.js
@@ -246,15 +246,16 @@ const vfjsHelpers = {
 
     return value;
   },
-  getVfjsFieldsModels(fields) {
-    return fields.reduce(
-      (models, { children = [], model }) => [
-        ...models,
-        ...(model && models.indexOf(model) === -1 ? [model] : []),
-        ...this.getVfjsFieldsModels(children),
-      ],
-      [],
-    );
+  getVfjsFieldsModels(fields, fieldModels = []) {
+    return fields.reduce((models, { children = [], model }) => {
+      if (model) {
+        if (models.indexOf(model) === -1) {
+          models.push(model);
+        }
+      }
+
+      return this.getVfjsFieldsModels(children, models);
+    }, fieldModels);
   },
 };
 
