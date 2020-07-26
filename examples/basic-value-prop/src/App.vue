9cc5715bd723b126210f4cf11e83fdb3576ee498
Tobias Järvelöv <tobias.jarvelov@gmail.com>
2020-07-26 22:44:47 +0200

    Add valueProp example

---
+++ examples/basic-value-prop/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <div class="wrapper">
+      <div class="container">
+        <BasicValueProp />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import BasicValueProp from "../../components/Examples/BasicValueProp.vue";
+
+export default {
+  name: "App",
+  components: {
+    BasicValueProp
+  }
+};
+</script>
