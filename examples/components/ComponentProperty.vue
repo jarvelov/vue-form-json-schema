c879c4a1bf0738f2f202bc65fcb71d07870d0805
Tobias Järvelöv <tobias.jarvelov@gmail.com>
2020-08-09 14:38:07 +0200

    Add component to get properties of other components

---
+++ examples/components/ComponentProperty.vue
@@ -0,0 +1,23 @@
+<template>
+  <span>{{ text }}</span>
+</template>
+
+<script>
+export default {
+  name: "component-property",
+  props: ["name", "value"],
+  computed: {
+    examples() {
+      return this.$examples();
+    },
+    text() {
+      const example = this.examples.find(({ value }) => value === this.name);
+      if (example) {
+        return example[this.value];
+      }
+
+      return null;
+    }
+  }
+};
+</script>
