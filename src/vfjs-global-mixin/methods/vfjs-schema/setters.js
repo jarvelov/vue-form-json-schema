469c48def846bb2244ccc4e7292e491cd3233209
Tobias Järvelöv <tobias.jarvelov@gmail.com>
2020-10-06 22:34:20 +0200

    Add second param 'silent' to update vfjsSchema without any other effects

--- src/vfjs-global-mixin/methods/vfjs-schema/setters.js
+++ src/vfjs-global-mixin/methods/vfjs-schema/setters.js
@@ -6,34 +6,36 @@ import {
 } from '../../../constants';
 
 const vfjsSchemaSetters = {
-  setVfjsSchema(value) {
+  setVfjsSchema(value, silent = false) {
     if (!isEqual(value, this.vfjsSchema)) {
       this.vfjsSchema = cloneDeep(value);
 
-      if (this.vfjsOptions.validate) {
-        const vfjsModel = this.getVfjsModel();
+      if (!silent) {
+        if (this.vfjsOptions.validate) {
+          const vfjsModel = this.getVfjsModel();
 
-        this.vfjsBus.$emit(VFJS_EVENT_MODEL_VALIDATE, {
-          vfjsModel,
-          cb: (vfjsState) => {
-            this.vfjsBus.$emit(VFJS_EVENT_STATE_UPDATE, {
-              value: vfjsState,
-              cb: () => {
-                this.vfjsBus.$emit(VFJS_EVENT_STATE_UPDATED, {
-                  cb: () => {
-                    this.setVfjsFields();
-                  },
-                });
-              },
-            });
-          },
-        });
-      } else {
-        this.vfjsBus.$emit(VFJS_EVENT_STATE_UPDATED, {
-          cb: () => {
-            this.setVfjsFields();
-          },
-        });
+          this.vfjsBus.$emit(VFJS_EVENT_MODEL_VALIDATE, {
+            vfjsModel,
+            cb: (vfjsState) => {
+              this.vfjsBus.$emit(VFJS_EVENT_STATE_UPDATE, {
+                value: vfjsState,
+                cb: () => {
+                  this.vfjsBus.$emit(VFJS_EVENT_STATE_UPDATED, {
+                    cb: () => {
+                      this.setVfjsFields();
+                    },
+                  });
+                },
+              });
+            },
+          });
+        } else {
+          this.vfjsBus.$emit(VFJS_EVENT_STATE_UPDATED, {
+            cb: () => {
+              this.setVfjsFields();
+            },
+          });
+        }
       }
     }
   },
