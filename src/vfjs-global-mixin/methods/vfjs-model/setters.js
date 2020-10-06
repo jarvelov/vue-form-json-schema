1902731c7bb9021e4ff95b261f903ad3919686e4
Tobias Järvelöv <tobias.jarvelov@gmail.com>
2020-10-06 22:33:51 +0200

    Emit VFJS_EVENT_STATE_UPDATED before VFJS_EVENT_MODEl_UPDATED

--- src/vfjs-global-mixin/methods/vfjs-model/setters.js
+++ src/vfjs-global-mixin/methods/vfjs-model/setters.js
@@ -3,6 +3,7 @@ import {
   VFJS_EVENT_FIELD_MODEL_UPDATE,
   VFJS_EVENT_MODEL_VALIDATE,
   VFJS_EVENT_STATE_UPDATE,
+  VFJS_EVENT_STATE_UPDATED,
   VFJS_EVENT_MODEL_UPDATED,
 } from '../../../constants';
 
@@ -24,7 +25,11 @@ const vfjsModelSetters = {
             this.vfjsBus.$emit(VFJS_EVENT_STATE_UPDATE, {
               value: newVfjsState,
               cb: () => {
-                this.vfjsBus.$emit(VFJS_EVENT_MODEL_UPDATED);
+                this.vfjsBus.$emit(VFJS_EVENT_STATE_UPDATED, {
+                  cb: () => {
+                    this.vfjsBus.$emit(VFJS_EVENT_MODEL_UPDATED);
+                  },
+                });
               },
             });
           },
