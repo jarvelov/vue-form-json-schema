ddd3b755fceb1e8433129fdf5cfe1dcfbf37da1b
Tobias Järvelöv <tobias.jarvelov@gmail.com>
2020-10-06 22:17:17 +0200

    Trigger validation and update state, then set fields

--- src/vfjs-global-mixin/methods/vfjs-ui/setters.js
+++ src/vfjs-global-mixin/methods/vfjs-ui/setters.js
@@ -1,5 +1,9 @@
 import { cloneDeep, isEqual } from 'lodash';
-import { VFJS_EVENT_STATE_UPDATED } from '../../../constants';
+import {
+  VFJS_EVENT_MODEL_VALIDATE,
+  VFJS_EVENT_STATE_UPDATE,
+  VFJS_EVENT_STATE_UPDATED,
+} from '../../../constants';
 
 const vfjsUiSetters = {
   setVfjsUiSchema(uiSchema = []) {
@@ -17,11 +21,31 @@ const vfjsUiSetters = {
     this.vfjsFieldsActive = this.getVfjsUiFieldsActive(this.vfjsUiSchema);
     this.vfjsFieldsActiveModels = this.getVfjsFieldsModels(this.vfjsFieldsActive);
 
-    this.vfjsBus.$emit(VFJS_EVENT_STATE_UPDATED, {
-      cb: () => {
-        this.setVfjsFields();
-      },
-    });
+    if (this.vfjsOptions.validate) {
+      const vfjsModel = this.getVfjsModel();
+
+      this.vfjsBus.$emit(VFJS_EVENT_MODEL_VALIDATE, {
+        vfjsModel,
+        cb: (vfjsState) => {
+          this.vfjsBus.$emit(VFJS_EVENT_STATE_UPDATE, {
+            value: vfjsState,
+            cb: () => {
+              this.vfjsBus.$emit(VFJS_EVENT_STATE_UPDATED, {
+                cb: () => {
+                  this.setVfjsFields();
+                },
+              });
+            },
+          });
+        },
+      });
+    } else {
+      this.vfjsBus.$emit(VFJS_EVENT_STATE_UPDATED, {
+        cb: () => {
+          this.setVfjsFields();
+        },
+      });
+    }
   },
   setVfjsFields() {
     this.vfjsFields = this.getVfjsFields(this.vfjsFieldsActive);
