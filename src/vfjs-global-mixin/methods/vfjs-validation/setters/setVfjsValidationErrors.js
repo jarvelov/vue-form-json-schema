685b51db03d9e9ed2e87294a75b6d0b7148fc3ab
Tobias Järvelöv <tobias.jarvelov@gmail.com>
2020-11-13 22:37:04 +0100

    Separate each setter into a separate file

--- src/vfjs-global-mixin/methods/vfjs-validation/setters/setVfjsValidationErrors.js
+++ src/vfjs-global-mixin/methods/vfjs-validation/setters/setVfjsValidationErrors.js
@@ -3,14 +3,14 @@ import {
   VFJS_EVENT_FIELD_STATE_UPDATE,
   VFJS_EVENT_MODEL_VALIDATE,
   VFJS_EVENT_UI_FIELDS_UPDATE,
-} from '../../../constants';
+} from '../../../../constants';
 
-const vfjsValidationSetters = {
-  setVfjsValidationErrors() {
-    this.vfjsBus.$emit(VFJS_EVENT_MODEL_VALIDATE, {
-      vfjsModel: this.getVfjsModel(),
-      cb: () => {
-        const validateRequired = key => new Promise((resolve, reject) => {
+function setVfjsValidationErrors() {
+  this.vfjsBus.$emit(VFJS_EVENT_MODEL_VALIDATE, {
+    vfjsModel: this.getVfjsModel(),
+    cb: () => {
+      const validateRequired = (key) =>
+        new Promise((resolve, reject) => {
           this.vfjsBus.$emit(VFJS_EVENT_FIELD_MODEL_VALIDATE, {
             key,
             value: this.getVfjsFieldModel(key),
@@ -30,11 +30,12 @@ const vfjsValidationSetters = {
           });
         });
 
-        const operations = this.vfjsFieldsRequired.map(validateRequired);
-        return Promise.all(operations).then(() => this.vfjsBus.$emit(VFJS_EVENT_UI_FIELDS_UPDATE));
-      },
-    });
-  },
-};
+      const operations = this.vfjsFieldsRequired.map(validateRequired);
+      return Promise.all(operations).then(() =>
+        this.vfjsBus.$emit(VFJS_EVENT_UI_FIELDS_UPDATE),
+      );
+    },
+  });
+}
 
-export default vfjsValidationSetters;
+export default setVfjsValidationErrors;
