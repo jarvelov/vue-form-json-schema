07975c5b4de8b33b42ceb36bb9d86c734ae300d7
Tobias Järvelöv <tobias.jarvelov@gmail.com>
2020-11-18 19:18:50 +0100

    Format

--- src/vfjs-global-mixin/methods/vfjs-ui/setters/setVfjsUiFieldsActive.js
+++ src/vfjs-global-mixin/methods/vfjs-ui/setters/setVfjsUiFieldsActive.js
@@ -6,7 +6,9 @@ import {
 
 function setVfjsUiFieldsActive() {
   this.vfjsFieldsActive = this.getVfjsUiFieldsActive(this.vfjsUiSchema);
-  this.vfjsFieldsActiveModels = this.vfjsHelperGetFieldsModels(this.vfjsFieldsActive);
+  this.vfjsFieldsActiveModels = this.vfjsHelperGetFieldsModels(
+    this.vfjsFieldsActive,
+  );
 
   if (this.vfjsOptions.validate) {
     const vfjsModel = this.getVfjsModel();
