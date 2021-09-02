8a503fee53cdd46b6fee222ae4c50b09903a562c
Tobias Järvelöv <tobias.jarvelov@gmail.com>
2021-09-02 15:45:16 +0200

    Use constants for declared props

--- src/vfjs-global-mixin/props.js
+++ src/vfjs-global-mixin/props.js
@@ -1,34 +1,45 @@
+import {
+  VFJS_EXTERNAL_COMPONENTS_PROP,
+  VFJS_EXTERNAL_ID_PROP,
+  VFJS_EXTERNAL_MODEL_PROP,
+  VFJS_EXTERNAL_OPTIONS_PROP,
+  VFJS_EXTERNAL_SCHEMA_PROP,
+  VFJS_EXTERNAL_SCHEMAS_PROP,
+  VFJS_EXTERNAL_TAG_PROP,
+  VFJS_EXTERNAL_UI_SCHEMA_PROP,
+} from '../constants';
+
 const props = {
-  components: {
+  [VFJS_EXTERNAL_COMPONENTS_PROP]: {
     type: Object,
     default: () => ({}),
   },
-  model: {
+  [VFJS_EXTERNAL_MODEL_PROP]: {
     type: Object,
     default: () => ({}),
   },
-  schema: {
+  [VFJS_EXTERNAL_SCHEMA_PROP]: {
     type: Object,
     required: true,
     default: () => ({}),
   },
-  schemas: {
+  [VFJS_EXTERNAL_SCHEMAS_PROP]: {
     type: Object,
     default: () => ({}),
   },
-  id: {
+  [VFJS_EXTERNAL_ID_PROP]: {
     type: String,
     default: () => Math.random().toString(36).substr(2, 9),
   },
-  options: {
+  [VFJS_EXTERNAL_OPTIONS_PROP]: {
     type: Object,
     default: () => ({}),
   },
-  tag: {
+  [VFJS_EXTERNAL_TAG_PROP]: {
     type: String,
     default: 'div',
   },
-  uiSchema: {
+  [VFJS_EXTERNAL_UI_SCHEMA_PROP]: {
     type: Array,
     required: true,
     default: () => ({}),
