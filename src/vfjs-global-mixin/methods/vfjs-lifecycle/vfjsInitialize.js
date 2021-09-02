92badf5e4a533e972766d84656225981c03ec406
Tobias Järvelöv <tobias.jarvelov@gmail.com>
2021-09-02 15:51:36 +0200

    Use prop constants

--- src/vfjs-global-mixin/methods/vfjs-lifecycle/vfjsInitialize.js
+++ src/vfjs-global-mixin/methods/vfjs-lifecycle/vfjsInitialize.js
@@ -2,20 +2,25 @@ import {
   VFJS_EVENT_MODEL_VALIDATE,
   VFJS_EVENT_STATE_UPDATE,
   VFJS_EVENT_STATE_UPDATED,
+  VFJS_EXTERNAL_COMPONENTS_PROP,
+  VFJS_EXTERNAL_MODEL_PROP,
+  VFJS_EXTERNAL_OPTIONS_PROP,
+  VFJS_EXTERNAL_SCHEMA_PROP,
+  VFJS_EXTERNAL_UI_SCHEMA_PROP,
 } from '../../../constants';
 
 function vfjsInitialize() {
   // Set the JSON schema
-  this.setVfjsSchema(this.schema, true);
+  this.setVfjsSchema(this[VFJS_EXTERNAL_SCHEMA_PROP], true);
 
   // Set up options
   this.vfjsOptions = {
     ...this.vfjsOptions,
-    ...this.options,
+    ...this[VFJS_EXTERNAL_OPTIONS_PROP],
   };
 
   // Set up the local components
-  this.vfjsComponents = this.components;
+  this.vfjsComponents = this[VFJS_EXTERNAL_COMPONENTS_PROP];
 
   // Set up the plugin's internal bus
   this.vfjsBusInitialize();
@@ -24,7 +29,7 @@ function vfjsInitialize() {
   this.addVfjsListeners(this.vfjsEvents, this.vfjsBusEventHandler);
 
   // Set the model from props
-  this.setVfjsModel(this.model, true);
+  this.setVfjsModel(this[VFJS_EXTERNAL_MODEL_PROP], true);
 
   // Set up validation
   this.vfjsValidationInitialize();
@@ -42,7 +47,7 @@ function vfjsInitialize() {
             this.vfjsBus.$emit(VFJS_EVENT_STATE_UPDATED, {
               cb: () => {
                 // Set up ui schema
-                this.setVfjsUiSchema(this.uiSchema);
+                this.setVfjsUiSchema(this[VFJS_EXTERNAL_UI_SCHEMA_PROP]);
               },
             });
           },
@@ -51,7 +56,7 @@ function vfjsInitialize() {
     });
   } else {
     // Set up ui schema
-    this.setVfjsUiSchema(this.uiSchema);
+    this.setVfjsUiSchema(this[VFJS_EXTERNAL_UI_SCHEMA_PROP]);
   }
 }
 
