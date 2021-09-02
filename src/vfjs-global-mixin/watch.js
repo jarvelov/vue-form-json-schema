3af7427d0b04d1a30c920ec713e399afc073732a
Tobias Järvelöv <tobias.jarvelov@gmail.com>
2021-09-02 15:45:07 +0200

    Use constants for watched props

--- src/vfjs-global-mixin/watch.js
+++ src/vfjs-global-mixin/watch.js
@@ -1,22 +1,29 @@
 import { merge } from 'lodash';
+import {
+  VFJS_EXTERNAL_COMPONENTS_PROP,
+  VFJS_EXTERNAL_MODEL_PROP,
+  VFJS_EXTERNAL_OPTIONS_PROP,
+  VFJS_EXTERNAL_SCHEMA_PROP,
+  VFJS_EXTERNAL_UI_SCHEMA_PROP,
+} from '../constants';
 
 const watch = {
-  components(value) {
+  [VFJS_EXTERNAL_COMPONENTS_PROP](value) {
     this.vfjsComponents = Object.assign({}, value);
   },
-  model: {
+  [VFJS_EXTERNAL_MODEL_PROP]: {
     deep: true,
     handler(value) {
       this.setVfjsModel(value);
     },
   },
-  schema(value) {
+  [VFJS_EXTERNAL_SCHEMA_PROP](value) {
     this.setVfjsSchema(value);
   },
-  uiSchema(value) {
+  [VFJS_EXTERNAL_UI_SCHEMA_PROP](value) {
     this.setVfjsUiSchema(value);
   },
-  options(value) {
+  [VFJS_EXTERNAL_OPTIONS_PROP](value) {
     this.vfjsOptions = merge({}, this.vfjsOptions, value);
 
     if (this.vfjsOptions.showValidationErrors) {
