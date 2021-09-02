3b2678dcb07da195f7038dafc3ae317d551dce7b
Tobias Järvelöv <tobias.jarvelov@gmail.com>
2021-09-02 15:57:43 +0200

    Just return the slots
    Vue 3 now wants slots as an object, which is what $slots is, so lets 
    just return it.

--- src/vfjs-field-mixin/methods/getters/getVfjsChildren.js
+++ src/vfjs-field-mixin/methods/getters/getVfjsChildren.js
@@ -1,19 +1,5 @@
-import vfjsHelperCreateElement from '../../../vfjs-global-mixin/methods/vfjs-helpers/vfjsHelperCreateElement';
-
 function getVfjsChildren() {
-  const { default: defaultSlot, ...slots } = this.$slots;
-
-  const namedSlots = Object.keys(slots).map((key) => [
-    vfjsHelperCreateElement(
-      'template',
-      {
-        slot: key,
-      },
-      slots[key],
-    ),
-  ]);
-
-  return [defaultSlot, ...namedSlots];
+  return this.$slots;
 }
 
 export default getVfjsChildren;
